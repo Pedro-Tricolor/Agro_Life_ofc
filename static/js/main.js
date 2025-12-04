9aee94f2956ab376abf0555a933870b5a460a698
Pedro-Tricolor <pedro.h.pereira31@aluno.senai.br>
2025-12-04 08:33:45 -0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,49 @@
+
+function main() {
+
+(function () {
+   'use strict';
+
+
+    // Show Menu on Book
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-default',
+        offset: 80
+    });
+
+	// Hide nav on click
+
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+    });
+
+
+	// Testimonial Slider
+	  	$(document).ready(function() {
+
+  	});
+
+}());
+
+
+}
+main();
